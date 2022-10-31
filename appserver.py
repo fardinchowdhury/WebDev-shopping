c512184dc8e8b42289c71eace728913d921223f0
Hazeorn <jkegordon6@gmail.com>
2022-10-31 08:42:52 -0400

    Made static folder, working on serving home/login page

--- appserver.py
+++ appserver.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('home.html')
+
