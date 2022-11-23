25c8fbcd7979a8d4e60053ed440924d7fb282c16
Hazeorn <jkegordon6@gmail.com>
2022-11-23 15:25:52 -0500

    Fixed log.html and sign.html, added redirects to proper content for logged in users

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import bcrypt
 from helper_functions import *
 from flask import Flask, render_template, url_for, request, flash, redirect, make_response
 
-
-
-
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key = int.to_bytes(3, secrets.randbits(24), byteorder="big")
 
@@ -20,11 +17,28 @@ transaction_records = database["transactions"]  # Holds a record of all previous
 
 @app.route('/')  # Serving home page and associated content
 def home():
+    if get_logged_in(request, user_table): return render_template('item.html', error=None)
     return render_template('home.html', error=None)
 
 
-@app.route('/signup', methods=["POST"]) # Handles sign-in requests
+@app.route('/login_page')
+def login_page():
+    if get_logged_in(request, user_table):
+        return redirect('/', 302, None)
+    return render_template('log.html')
+
+
+@app.route('/signup_page')
+def signup_page():
+    if get_logged_in(request, user_table):
+        return redirect('/', 302, None)
+    return render_template('sign.html')
+
+
+@app.route('/signup', methods=["POST"])  # Handles sign-in requests
 def signup():
+    if get_logged_in(request, user_table):
+        return redirect('/', 302, None)
     username = request.form.get("register_username")
     password = request.form.get("register_password")
     email = request.form.get("register_email")
@@ -33,13 +47,17 @@ def signup():
         print("Creating user!", flush=True)
         hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
         user_table.insert_one({"user": username, "email": email, "pass": hashed_password})
+        flash("Signup successful!")
         return render_template('home.html')
     else:
         flash("Email or username already in use!")
-        return redirect("/", 302, None)
+        return redirect(request.referrer, 302, None)
 
-@app.route('/signup', methods=["POST"]) # Login handler
+
+@app.route('/login', methods=["POST"])  # Login handler
 def login():
+    if get_logged_in(request, user_table):
+        return redirect('/', 302, None)
     email = request.form.get("login_email")
     password = request.form.get("login_password")
     # Checking if the user is actually registered to us
@@ -50,11 +68,11 @@ def login():
     else:
         # Then we authenticate their password
         hashed = user["pass"]
-        if bcrypt.checkpw(password.encode(),hashed):
+        if bcrypt.checkpw(password.encode(), hashed):
             auth_token = secrets.token_urlsafe()
             hashed_token = bcrypt.hashpw(auth_token.encode(), bcrypt.gensalt(1))
             user_table.update_one({"email": email}, {"$set": {"token": hashed_token}})
-            response = make_response(render_template()) # FILL THIS OUT TO LEAD TO THE FIRST PAGE
+            response = make_response(render_template())  # FILL THIS OUT TO LEAD TO THE FIRST PAGE
             response.set_cookie('auth_token', value=auth_token, httponly=True, max_age=604800)
             response.set_cookie('email', value=email, httponly=True, max_age=604800)
             return response
@@ -63,11 +81,12 @@ def login():
             flash("Invalid credentials!")
             return redirect("/", 302, None)
 
+
 @app.route('/logout', methods=["POST"])
 def logout():
     if get_logged_in(request, user_table):
         # Help with deleting cookies from https://sparkdatabox.com/tutorials/python-flask/delete-cookies
-        response = make_response(render_template())  # FILL THIS OUT TO LEAD TO THE LOGIN PAGE
+        response = make_response(render_template('home.html', error=None))  # FILL THIS OUT TO LEAD TO THE LOGIN PAGE
         response.set_cookie('auth_token', value="", httponly=True, max_age=0)
         response.set_cookie('email', value="", httponly=True, max_age=0)
         return response
@@ -75,7 +94,5 @@ def logout():
         return redirect("/", 302, None)
 
 
-
 if __name__ == "__main__":
-
     app.run(debug=True, host='0.0.0.0')
